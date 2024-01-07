ee8b337bc51d9fa38384ac6a1594e3e0136840f6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-07 22:38:15 +0500

    tablelarni qoshdim

---
+++ src/Agriculture.Domain/Entities/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Domain.Entities
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+
+      
+       
+
+
+    }
+}
