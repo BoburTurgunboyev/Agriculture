4c7dcfd6ccef8224dfafdfc10c5428b1b45e0378
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-07 22:38:37 +0500

    tablelarni qoshdim

---
+++ src/Agriculture.Domain/Entities/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Domain.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int Howmany { get; set; }
+        public decimal SumTotal { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
