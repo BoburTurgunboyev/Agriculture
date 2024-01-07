4cbbfebfb92b3703492639eddfa878e108c91c5c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-07 22:38:24 +0500

    tablelarni qoshdim

---
+++ src/Agriculture.Domain/Entities/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Domain.Entities
+{
+    public class ProductService
+    {
+        public int Id { get; set; }
+        public string DairyProducts { get; set; }
+        public string StoreServices { get; set; }
+        public string DeliveryServices { get; set; }
+        public string AgriculturalServices { get; set; }
+        public string OrganicProducts { get; set; }
+        public string FreshVegetables { get; set; }
+        public Product Product { get; set; }
+    }
+}
