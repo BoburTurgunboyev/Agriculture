06e5aef4680becd9d511f048379ba63cb995fe62
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-07 22:38:32 +0500

    tablelarni qoshdim

---
+++ src/Agriculture.Domain/Entities/Cotegory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Domain.Entities
+{
+    public class Cotegory
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Image { get; set; }
+       
+    }
+}
