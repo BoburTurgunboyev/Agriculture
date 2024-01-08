4e60692262774bdb02ee509ecad3395d0c9cad20
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 16:39:35 +0500

    databasaga ulandi

---
+++ src/Agriculture.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Infrastructure.Data
+{
+    public class AppDbContext:DbContext,IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Shop> shops { get; set; }
+
+        async ValueTask<int> IAppDbContext.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken); 
+        }
+    }
+}
