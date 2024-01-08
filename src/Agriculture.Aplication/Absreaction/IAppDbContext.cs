362d0802743bfd74d189a7d0157f2d57a61c2846
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 16:39:57 +0500

    databasaga ulandi

---
+++ src/Agriculture.Aplication/Absreaction/IAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Agriculture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.Absreaction
+{
+    public interface IAppDbContext
+    {
+        public DbSet<Shop> shops { get; set; }
+        public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
