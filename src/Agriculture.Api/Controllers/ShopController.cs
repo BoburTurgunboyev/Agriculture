fd218fd07716897903ec637a5c4d4565ab2fc89b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 17:18:53 +0500

    shop cud

---
+++ src/Agriculture.Api/Controllers/ShopController.cs
@@ -0,0 +1,37 @@
+﻿using Agriculture.Aplication.UseCases.ShopCase.commands;
+using Agriculture.Aplication.UseCases.ShopCase.Dtos;
+using Agriculture.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agriculture.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ShopController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateShop(ShopDto shopdto)
+        {
+            var shop = new CreateShopCommand()
+            {
+                Name = shopdto.Name,
+                Adress = shopdto.Adress,
+                Phone = shopdto.Phone,
+                Email = shopdto.Email,
+            };
+
+            var result = await _mediator.Send(shop);
+            return Ok(result);  
+        }
+    }
+}
