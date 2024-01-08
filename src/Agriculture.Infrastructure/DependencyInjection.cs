c5054dfdb3c72662af87ceb9db189efba93678fa
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 16:39:07 +0500

    databasaga ulandi

---
+++ src/Agriculture.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnections"));
+            });
+
+            return services;
+        }
+    }
+}
