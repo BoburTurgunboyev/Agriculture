8deb8c8bb6639e323452d5ef26152fe99253c75d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 17:18:35 +0500

    shop cud

---
+++ src/Agriculture.Aplication/UseCases/ShopCase/Commands/UpdateShopCommand.cs
@@ -0,0 +1,16 @@
+﻿using Agriculture.Aplication.UseCases.ShopCase.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ShopCase.Commands
+{
+    public class UpdateShopCommand :ShopDto,IRequest<bool>
+    {
+        public int Id { get; set; }
+
+    }
+}
