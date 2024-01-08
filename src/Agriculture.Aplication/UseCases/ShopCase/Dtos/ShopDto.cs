d59ea1445556be92b6e9c2c704d018c454631814
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 17:18:31 +0500

    shop cud

---
+++ src/Agriculture.Aplication/UseCases/ShopCase/Dtos/ShopDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ShopCase.Dtos
+{
+    public class ShopDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
