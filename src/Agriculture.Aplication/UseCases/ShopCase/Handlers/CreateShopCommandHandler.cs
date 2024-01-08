c48b16bf7c54d77b323ec076c04baa5e79a49e90
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-08 17:18:26 +0500

    shop cud

---
+++ src/Agriculture.Aplication/UseCases/ShopCase/Handlers/CreateShopCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ShopCase.commands;
+using Agriculture.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ShopCase.Handlers
+{
+    public class CreateShopCommandHandler : IRequestHandler<CreateShopCommand, bool>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public CreateShopCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(CreateShopCommand request, CancellationToken cancellationToken)
+        {
+            var shop = new Shop()
+            {
+                Name = request.Name,
+                Adress = request.Adress,
+                Phone = request.Phone,
+                Email = request.Email,
+
+            };
+            await _appDbContext.shops.AddAsync(shop);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result>0;
+        }
+    }
+}
