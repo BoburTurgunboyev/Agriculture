16054932661d56cc9da5532557d9e93289f03d61
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:13:29 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/CartCase/Dtos/CartDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.CartCase.Dtos
+{
+    internal class CartDto
+    {
+        public int Quentity { get; set; }
+        public decimal SumTotal { get; set; }
+    }
+}
