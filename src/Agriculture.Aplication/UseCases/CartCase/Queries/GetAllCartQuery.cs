bd675bf68e46c28dfc8e03719adc00bdc5abc838
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:13:26 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/CartCase/Queries/GetAllCartQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.CartCase.Queries
+{
+    internal class GetAllCartQuery
+    {
+    }
+}
