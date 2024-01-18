73e052078a3551adf3c585ecfa74cf46802a2925
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:11:22 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/ProductNewsCase/Dtos/ProductNewsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ProductNewsCase.Dtos
+{
+    internal class ProductNewsDto
+    {
+        public string Image { get; set; }
+        public string Vitamin { get; set; }
+        public string VitaminDescription { get; set; }
+    }
+}
