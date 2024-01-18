0ef002e97e11a7f8011d60000a911e35a9ad70eb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:13:08 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/ShopCase/Queries/GetAllShopQuery.cs
@@ -0,0 +1,14 @@
+﻿using Agriculture.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ShopCase.Queries
+{
+    public class GetAllShopQuery :IRequest<List<Shop>>
+    {
+    }
+}
