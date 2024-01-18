63017930786aa99bc80279d9774af19bb469c4ae
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:11:52 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/ProductCase/Dtos/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ProductCase.Dtos
+{
+    internal class ProductDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
