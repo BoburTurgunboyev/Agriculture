df92d37d469b76dead41256da134a36c99f4665a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:08:23 +0500

    UseCases Added

--- src/Agriculture.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ src/Agriculture.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -37,10 +37,6 @@ namespace Agriculture.Infrastructure.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("Phone")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
