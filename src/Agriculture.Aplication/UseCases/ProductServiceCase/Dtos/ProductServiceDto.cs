ce02a68f2d4d2a9e359137bb6c81c07b7be92788
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:10:25 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/ProductServiceCase/Dtos/ProductServiceDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.ProductServiceCase.Dtos
+{
+    internal class ProductServiceDto
+    {
+        public string DairyProducts { get; set; }
+        public string StoreServices { get; set; }
+        public string DeliveryServices { get; set; }
+        public string AgriculturalServices { get; set; }
+        public string OrganicProducts { get; set; }
+        public string FreshVegetables { get; set; }
+    }
+}
