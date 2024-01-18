10b26f753cce6aa90af98fc55b955e74bbbd3d9b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 01:09:18 +0500

    UseCases Added

---
+++ src/Agriculture.Aplication/UseCases/UserCase/Dtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.UseCases.UserCase.Dtos
+{
+    public class UserDto
+    {
+        public string FullName { get; set; }
+        public string Image { get; set; }
+        public string Job { get; set; }
+
+    }
+}
