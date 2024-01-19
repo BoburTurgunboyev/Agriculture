f0d94d79be4a361a4b7d7bd6fd54355a2b7df536
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:49:38 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductCase/Handlers/CreateProductCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductCase/Handlers/CreateProductCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductCase.Commands;
+using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,27 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductCase.Handlers
 {
-    internal class CreateProductCommandHandler
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public CreateProductCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product()
+            {
+                Name = request.Name,
+                Image = request.Image,
+                Price = request.Price
+            };
+
+            _appDbContext.products.Add(product);
+            var res = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
     }
 }
