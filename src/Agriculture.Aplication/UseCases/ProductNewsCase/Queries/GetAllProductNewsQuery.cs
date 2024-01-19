64b9f4b08eeecfa8ed1975c05e021830ddf53a48
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:52:10 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductNewsCase/Queries/GetAllProductNewsQuery.cs
+++ src/Agriculture.Aplication/UseCases/ProductNewsCase/Queries/GetAllProductNewsQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductNewsCase.Queries
 {
-    internal class GetAllProductNewsQuery
+    public class GetAllProductNewsQuery : IRequest<List<ProductNews>>
     {
     }
 }
