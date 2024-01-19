713e8af4f276f32931e9615644e54e2927364635
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:50:48 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductNewsCase/Handlers/CreateProductNewsCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductNewsCase/Handlers/CreateProductNewsCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductNewsCase.Commands;
+using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,27 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductNewsCase.Handlers
 {
-    internal class CreateProductNewsCommandHandler
+    public class CreateProductNewsCommandHandler : IRequestHandler<CreateProductNewsCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public CreateProductNewsCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(CreateProductNewsCommand request, CancellationToken cancellationToken)
+        {
+            var res = new ProductNews() 
+            {
+                Image = request.Image,
+                Vitamin=request.Vitamin,
+                VitaminDescription=request.VitaminDescription
+            };
+            
+            _appDbContext.productsNews.Add(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
