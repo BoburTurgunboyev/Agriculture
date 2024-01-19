1c04da6876a42c30e3b1106303ad743fefb2a29b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:52:37 +0500

    cases

--- src/Agriculture.Aplication/UseCases/CartCase/Commands/CreateCartComand.cs
+++ src/Agriculture.Aplication/UseCases/CartCase/Commands/CreateCartComand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Agriculture.Aplication.UseCases.CartCase.Dtos;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.CartCase.Commands
 {
-    public class CreateCartComand
+    public class CreateCartComand :CartDto,IRequest<bool>
     {
     }
 }
