f8e2eeb42b139f6c5edd8d1033274c9f5a6c72d5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:49:34 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductCase/Commands/DeleteProductCommand.cs
+++ src/Agriculture.Aplication/UseCases/ProductCase/Commands/DeleteProductCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductCase.Commands
 {
-    internal class DeleteProductCommand
+    public class DeleteProductCommand:IRequest<bool>
     {
+        public int Id { get; set; } 
     }
 }
