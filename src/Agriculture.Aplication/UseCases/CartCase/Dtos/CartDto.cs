a8b27391bcd102ca761055f4d715f97ea16ea122
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:52:23 +0500

    cases

--- src/Agriculture.Aplication/UseCases/CartCase/Dtos/CartDto.cs
+++ src/Agriculture.Aplication/UseCases/CartCase/Dtos/CartDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.CartCase.Dtos
 {
-    internal class CartDto
+    public class CartDto
     {
         public int Quentity { get; set; }
         public decimal SumTotal { get; set; }
