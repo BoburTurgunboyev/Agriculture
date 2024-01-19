6e64edc0dd913449e72e06b43793e9f02d2c4acc
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:49:53 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductCase/Handlers/GetByIdProductQueryHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductCase/Handlers/GetByIdProductQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductCase.Queries;
+using Agriculture.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,19 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductCase.Handlers
 {
-    internal class GetByIdProductQueryHandler
+    public class GetByIdProductQueryHandler : IRequestHandler<GetByIdProductQuery, Product>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetByIdProductQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Product> Handle(GetByIdProductQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.products.FirstOrDefaultAsync(x=>x.Id==request.Id);
+            return res;
+        }
     }
 }
