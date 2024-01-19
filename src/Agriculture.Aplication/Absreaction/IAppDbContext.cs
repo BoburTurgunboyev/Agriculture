c03b21633a2361de32d1467fe02303f702c2ca58
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:52:41 +0500

    cases

--- src/Agriculture.Aplication/Absreaction/IAppDbContext.cs
+++ src/Agriculture.Aplication/Absreaction/IAppDbContext.cs
@@ -11,6 +11,10 @@ namespace Agriculture.Aplication.Absreaction
     public interface IAppDbContext
     {
         public DbSet<Shop> shops { get; set; }
+        public DbSet<Cart> carts { get; set; }
+        public DbSet<Product> products { get; set; } 
+        public DbSet<ProductNews> productsNews { get; set; }
+        public DbSet<ProductService> productsService { get; set; }  
         public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
