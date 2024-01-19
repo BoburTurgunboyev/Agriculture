3106d54926d118803ce41cfdfa877c82f2b6a66a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:51:27 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/UpdateProductServiceCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/UpdateProductServiceCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductServiceCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,26 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductServiceCase.Handlers
 {
-    internal class UpdateProductServiceCommandHandler
+    public class UpdateProductServiceCommandHandler : IRequestHandler<UpdateProductServiceCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public UpdateProductServiceCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(UpdateProductServiceCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.productsService.FirstOrDefaultAsync(x=>x.Id==request.Id);
+            if (res!=null)
+            {
+                return false;
+            }
+
+            _appDbContext.productsService.Update(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
