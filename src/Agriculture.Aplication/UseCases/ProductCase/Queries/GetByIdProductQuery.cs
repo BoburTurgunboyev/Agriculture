39ab08803e198713ae99e8bec27c6a725ae4fc74
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:50:16 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductCase/Queries/GetByIdProductQuery.cs
+++ src/Agriculture.Aplication/UseCases/ProductCase/Queries/GetByIdProductQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductCase.Queries
 {
-    internal class GetByIdProductQuery
+    public class GetByIdProductQuery:IRequest<Product>
     {
+        public int Id { get; set; }
     }
 }
