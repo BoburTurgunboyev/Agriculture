5de1bef1c9a610cd07ea92a9c179538d84bf9661
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:51:45 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/CreateProductServiceCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/CreateProductServiceCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductServiceCase.Commands;
+using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,29 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductServiceCase.Handlers
 {
-    internal class CreateProductServiceCommandHandler
+    public class CreateProductServiceCommandHandler : IRequestHandler<CreateProductServiceCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public CreateProductServiceCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(CreateProductServiceCommand request, CancellationToken cancellationToken)
+        {
+            var res = new ProductService()
+            {
+                DairyProducts = request.DairyProducts,
+                StoreServices = request.StoreServices,
+                DeliveryServices = request.DeliveryServices,
+                AgriculturalServices = request.AgriculturalServices,
+                OrganicProducts = request.OrganicProducts,
+                FreshVegetables = request.FreshVegetables
+            };
+            _appDbContext.productsService.Add(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
