92775f8afd64f178785bfe5d790a7f555aeb9ae3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:52:18 +0500

    cases

--- src/Agriculture.Aplication/UseCases/CartCase/Handlers/UpdateCartCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/CartCase/Handlers/UpdateCartCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.CartCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,31 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.CartCase.Handlers
 {
-    internal class UpdateCartCommandHandler
+    public class UpdateCartCommandHandler : IRequestHandler<UpdateCartCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public UpdateCartCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(UpdateCartCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.carts.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+
+            res.Quentity=request.Quentity;
+            res.SumTotal = request.SumTotal;
+
+            _appDbContext.carts.Update(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+
+            
+        }
     }
 }
