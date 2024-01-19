3b354dc73317050b7e54231cf5a5c5989feb6ed4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:51:12 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductNewsCase/Handlers/UpdateProductNewsCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductNewsCase/Handlers/UpdateProductNewsCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductNewsCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,32 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductNewsCase.Handlers
 {
-    internal class UpdateProductNewsCommandHandler
+    public class UpdateProductNewsCommandHandler : IRequestHandler<UpdateProductNewsCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public UpdateProductNewsCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(UpdateProductNewsCommand request, CancellationToken cancellationToken)
+        {
+           var res =await _appDbContext.productsNews.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+
+            res.Image=request.Image;
+            res.Vitamin=request.Vitamin;
+            res.VitaminDescription=request.VitaminDescription; 
+            
+            _appDbContext.productsNews.Remove(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+
+
     }
 }
