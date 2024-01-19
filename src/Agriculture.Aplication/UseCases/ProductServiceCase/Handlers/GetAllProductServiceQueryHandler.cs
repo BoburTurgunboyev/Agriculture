0b95fe2dbeff667c335ac240f185fa66968bd385
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:51:36 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/GetAllProductServiceQueryHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/GetAllProductServiceQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductServiceCase.Queries;
+using Agriculture.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductServiceCase.Handlers
 {
-    internal class GetAllProductServiceQueryHandler
+    public class GetAllProductServiceQueryHandler : IRequestHandler<GetAllProductServiceQuery, List<ProductService>>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetAllProductServiceQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<ProductService>> Handle(GetAllProductServiceQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbContext.productsService.ToListAsync(cancellationToken);
+            return result;
+        }
     }
 }
