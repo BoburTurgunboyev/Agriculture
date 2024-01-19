9797ac1a8b422cd0a36245e88981cf6691b938f6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-19 18:51:41 +0500

    cases

--- src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/DeleteProductServiceCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductServiceCase/Handlers/DeleteProductServiceCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ProductServiceCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,27 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ProductServiceCase.Handlers
 {
-    internal class DeleteProductServiceCommandHandler
+    public class DeleteProductServiceCommandHandler : IRequestHandler<DeleteProductServiceCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public DeleteProductServiceCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(DeleteProductServiceCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.productsService.FirstOrDefaultAsync(x=>x.Id==request.Id);
+            if (res!=null)
+            {
+                return false;
+            }
+
+            _appDbContext.productsService.Remove(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+            
+        }
     }
 }
