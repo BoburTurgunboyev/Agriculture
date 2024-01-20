6ccda8b6d2774770b65c908d9860ca16bbfd998a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-20 20:44:55 +0500

    modelda ozgarish

--- src/Agriculture.Domain/Entities/Cart.cs
+++ src/Agriculture.Domain/Entities/Cart.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Domain.Entities
 {
-    public class Cart
+    internal class Cart
     {
         public int Id { get; set; }
         public int Quentity { get; set; }
