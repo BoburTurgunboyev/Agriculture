82737d9c6e5aa6f2123717a04599439b2c0635a6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-20 20:47:01 +0500

    modellarda ozgarish bor

--- src/Agriculture.Domain/Entities/User.cs
+++ src/Agriculture.Domain/Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Domain.Entities
 {
-    internal class User
+    public class User
     {
         public int Id { get; set; }
         public string FullName { get; set; }
