f78538e07761a8c6cbe4ea58e5f22a61bcfeb378
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-20 20:46:45 +0500

    modellarda ozgarish bor

--- src/Agriculture.Domain/Entities/Product.cs
+++ src/Agriculture.Domain/Entities/Product.cs
@@ -2,7 +2,7 @@
 
 namespace Agriculture.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
