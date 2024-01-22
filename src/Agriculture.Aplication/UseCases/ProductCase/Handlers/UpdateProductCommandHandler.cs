c83abf4c3896ff88c9b9fc3f9daf074b6c1ac1ca
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 20:26:19 +0500

    file qoshildi

--- src/Agriculture.Aplication/UseCases/ProductCase/Handlers/UpdateProductCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ProductCase/Handlers/UpdateProductCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.FileSercives.ITTradeSoft.Application.FileServices;
 using Agriculture.Aplication.UseCases.ProductCase.Commands;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -13,24 +14,27 @@ namespace Agriculture.Aplication.UseCases.ProductCase.Handlers
     public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, bool>
     {
         private readonly IAppDbContext _appDbContext;
+        private readonly IFileService _fileService;
 
-        public UpdateProductCommandHandler(IAppDbContext appDbContext)
+        public UpdateProductCommandHandler(IAppDbContext appDbContext, IFileService fileService)
         {
             _appDbContext = appDbContext;
+            _fileService = fileService;
         }
 
         public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
         {
+            string productimage = await _fileService.UploadImageAsync(request.Image);
             var res = await _appDbContext.products.FirstOrDefaultAsync(x=>x.Id ==request.Id);
             if (res == null)
             {
                 return false;
             }
 
-            res.Name = request.Name;    
-            res.Image = request.Image;
+            res.Name = request.Name;
+            res.Image = productimage;
             res.Price = request.Price;
-            res.CartId = request.CartId;    
+             
 
             _appDbContext.products.Update(res);
             var result = await _appDbContext.SaveChangesAsync(cancellationToken);
