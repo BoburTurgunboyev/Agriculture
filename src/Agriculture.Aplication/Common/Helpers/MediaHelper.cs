e059a027e488d152c943e67099391cbec72907aa
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 20:27:07 +0500

    file qoshildi

---
+++ src/Agriculture.Aplication/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Aplication.Common.Helpers
+{
+    namespace ITTradeSoft.Application.Common.Helpers
+    {
+        public class MediaHelper
+        {
+            public static string MakeImageName(string filename)
+            {
+                FileInfo fileInfo = new FileInfo(filename);
+
+                string[] ImageExtension = GetImageExtensions();
+
+                if (ImageExtension.Any(x => x == fileInfo.Extension))
+                {
+                    string extension = fileInfo.Extension;
+                    string name = "IMG_" + Guid.NewGuid() + extension;
+                    return name;
+                }
+                throw new Exception();
+            }
+
+            public static string MakeFileName(string filename)
+            {
+                FileInfo fileInfo = new FileInfo(filename);
+
+                string[] ImageExtension = GetFileExtensions();
+
+                if (ImageExtension.Any(x => x == fileInfo.Extension))
+                {
+                    string extension = fileInfo.Extension;
+                    string name = "FILE_" + Guid.NewGuid() + extension;
+                    return name;
+                }
+                throw new Exception();
+            }
+
+            public static string[] GetImageExtensions()
+            {
+                return new string[]
+                {
+            ".jpg", ".jpeg",
+            ".png",
+            ".bmp",
+            ".svg"
+                };
+            }
+
+            public static string[] GetFileExtensions()
+            {
+                return new string[]
+                {
+            ".doc",
+            ".docx",
+            ".xls",
+            ".xlsx",
+            ".pdf",
+            ".txt",
+            ".zip",
+            ".rar",
+                };
+            }
+        }
+    }
+
+}
