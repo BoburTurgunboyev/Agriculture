b1be4cc0d316f428e0f8021f166b1e6167c153d4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:07 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/UserCase/Handlers/GetAllUsersQueryHandler.cs
+++ src/Agriculture.Aplication/UseCases/UserCase/Handlers/GetAllUsersQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.UserCase.Queries;
+using Agriculture.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.UserCase.Handlers
 {
-    public class GetAllUsersQueryHandler
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, List<User>>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetAllUsersQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.users.ToListAsync(cancellationToken);
+            return res;
+        }
     }
 }
