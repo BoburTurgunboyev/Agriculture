14b5db8b8ecdb74bd8adcfc56541247491a9fabc
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:00:53 +0500

    controllers added

---
+++ src/Agriculture.Infrastructure/Configration/ProductServiceConfigration.cs
@@ -0,0 +1,39 @@
+﻿using Agriculture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Infrastructure.Configration
+{
+    public class ProductServiceConfigration : IEntityTypeConfiguration<ProductService>
+    {
+        public void Configure(EntityTypeBuilder<ProductService> entity)
+        {
+            entity.HasKey(ps => ps.Id);
+
+            entity.Property(ps => ps.DairyProducts)
+                .IsRequired();
+
+            entity.Property(ps => ps.StoreServices)
+                .IsRequired();
+
+            entity.Property(ps => ps.DeliveryServices)
+                .IsRequired();
+
+            entity.Property(ps => ps.AgriculturalServices)
+                .IsRequired();
+
+            entity.Property(ps => ps.OrganicProducts)
+                .IsRequired();
+
+            entity.Property(ps => ps.FreshVegetables)
+                .IsRequired();
+
+     
+        }
+    }
+}
