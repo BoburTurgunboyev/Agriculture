9a3de32795f2febaa6a476283c147c70310d7dc8
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:03:26 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/ProductCase/Dtos/ProductDto.cs
+++ src/Agriculture.Aplication/UseCases/ProductCase/Dtos/ProductDto.cs
@@ -11,5 +11,7 @@ namespace Agriculture.Aplication.UseCases.ProductCase.Dtos
         public string Name { get; set; }
         public string Image { get; set; }
         public decimal Price { get; set; }
+        public int CartId { get; set; }
+     
     }
 }
