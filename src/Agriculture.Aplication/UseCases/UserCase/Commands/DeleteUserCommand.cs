1aa1f6b5fd5e810d3a983d43de9e083fdc2587ab
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:25 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/UserCase/Commands/DeleteUserCommand.cs
+++ src/Agriculture.Aplication/UseCases/UserCase/Commands/DeleteUserCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.UserCase.Commands
 {
-    internal class DeleteUserCommand
+    public class DeleteUserCommand:IRequest<bool>
     {
+        public int Id { get; set; }
     }
 }
