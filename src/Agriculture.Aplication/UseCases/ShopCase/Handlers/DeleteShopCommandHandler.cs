5a3a4d8a36148015cd75bea6586fa02026620ceb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:51 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/ShopCase/Handlers/DeleteShopCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ShopCase/Handlers/DeleteShopCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ShopCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ShopCase.Handlers
 {
-    internal class DeleteShopCommandHandler
+    public class DeleteShopCommandHandler : IRequestHandler<DeleteShopCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public DeleteShopCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(DeleteShopCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbContext.shops.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null)
+            {
+                return false;
+            }
+            _appDbContext.shops.Remove(result);
+            var res = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
     }
 }
