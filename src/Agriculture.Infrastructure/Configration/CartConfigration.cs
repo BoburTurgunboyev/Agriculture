26927092f34ac06e1d3f2c9887c06038e96c75b3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:00:48 +0500

    controllers added

---
+++ src/Agriculture.Infrastructure/Configration/CartConfigration.cs
@@ -0,0 +1,31 @@
+﻿using Agriculture.Aplication.UseCases.ProductCase.Commands;
+using Agriculture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Infrastructure.Configration
+{
+    public class CartConfigration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> entity)
+        {
+            entity.HasKey(c => c.Id);
+
+            entity.Property(c => c.Quentity)
+                .IsRequired();
+
+            entity.Property(c => c.SumTotal)
+                .HasColumnType("decimal(18, 2)")
+                .IsRequired();
+
+          
+
+        } 
+    
+    }
+}
