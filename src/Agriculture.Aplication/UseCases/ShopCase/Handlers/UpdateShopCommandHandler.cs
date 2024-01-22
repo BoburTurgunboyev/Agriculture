4df38c1f5ed57325b372b96006b2da5cc442d2af
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:39 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/ShopCase/Handlers/UpdateShopCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/ShopCase/Handlers/UpdateShopCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.ShopCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,29 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ShopCase.Handlers
 {
-    internal class UpdateShopCommandHandler
+    public class UpdateShopCommandHandler : IRequestHandler<UpdateShopCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public UpdateShopCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(UpdateShopCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.shops.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+            res.Email=request.Email;
+            res.Phone=request.Phone;
+            res.Address=request.Address;
+
+            _appDbContext.shops.Update(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
