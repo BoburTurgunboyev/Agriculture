d55c69091edeffe47cf20d3692297559c8ee3828
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:02:39 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/ShopCase/Queries/GetByIdShopQuery.cs
+++ src/Agriculture.Aplication/UseCases/ShopCase/Queries/GetByIdShopQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.ShopCase.Queries
 {
-    internal class GetByIdShopQuery
+    public class GetByIdShopQuery:IRequest<Shop>
     {
+        public int Id { get; set; } 
     }
 }
