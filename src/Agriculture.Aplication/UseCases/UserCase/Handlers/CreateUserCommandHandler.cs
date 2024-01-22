f295f116bda33062991c0a80b76a39be5945d0d1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:15 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/UserCase/Handlers/CreateUserCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/UserCase/Handlers/CreateUserCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.UserCase.Commands;
+using Agriculture.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,27 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.UserCase.Handlers
 {
-    internal class CreateUserCommandHandler
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public CreateUserCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User()
+            {
+                FullName=request.FullName,
+                Image=request.Image,
+                Job=request.Job,
+            };
+
+            _appDbContext.users.Add(user);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result>0;
+        }
     }
 }
