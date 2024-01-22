9943f3dbc005312bb24317e10f60571dabacb9da
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:11 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/UserCase/Handlers/DeleteUserCommandHandler.cs
+++ src/Agriculture.Aplication/UseCases/UserCase/Handlers/DeleteUserCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.UserCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.UserCase.Handlers
 {
-    public class DeleteUserCommandHandler
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public DeleteUserCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+            _appDbContext.users.Remove(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
