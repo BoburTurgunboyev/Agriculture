15750cbc4e78d28365beaf2348c421691e8c5918
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 20:26:56 +0500

    file qoshildi

--- src/Agriculture.Aplication/FileSercives/IFileService.cs
+++ src/Agriculture.Aplication/FileSercives/IFileService.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.FileSercives
 {
-    internal interface IFileService
+    using Microsoft.AspNetCore.Http;
+
+    namespace ITTradeSoft.Application.FileServices
     {
+        public interface IFileService
+        {
+            public ValueTask<string> UploadImageAsync(IFormFile imagepath);
+            public ValueTask<byte[]> GetImageAsync(string path);
+            public ValueTask<string> UploadFileAsync(IFormFile filePath);
+        }
     }
+
 }
