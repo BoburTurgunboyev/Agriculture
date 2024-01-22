a760ec0ac1dea55592d2fcf5d92db93caf5a33b0
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:00:57 +0500

    controllers added

---
+++ src/Agriculture.Infrastructure/Configration/ProductConfigration.cs
@@ -0,0 +1,31 @@
+﻿using Agriculture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Infrastructure.Configration
+{
+    public class ProductConfigration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> entity)
+        {
+            entity.HasKey(p => p.Id);
+
+            entity.Property(p => p.Name)
+                .IsRequired();
+
+            entity.Property(p => p.Image)
+                .IsRequired();
+
+            entity.Property(p => p.Price)
+                .HasColumnType("decimal(18, 2)")
+                .IsRequired();
+
+           
+        }
+    }
+}
