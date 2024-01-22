9a980e155ed65b9c7bf3677a40326d31d1a4050e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-22 15:01:34 +0500

    controllers added

--- src/Agriculture.Aplication/UseCases/UserCase/Handlers/GetByIdUserQueryHandler.cs
+++ src/Agriculture.Aplication/UseCases/UserCase/Handlers/GetByIdUserQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Agriculture.Aplication.Absreaction;
+using Agriculture.Aplication.UseCases.UserCase.Queries;
+using Agriculture.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,19 @@ using System.Threading.Tasks;
 
 namespace Agriculture.Aplication.UseCases.UserCase.Handlers
 {
-    internal class GetByIdUserQueryHandler
+    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, User>
     {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetByIdUserQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<User> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            var result  = await _appDbContext.users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
     }
 }
